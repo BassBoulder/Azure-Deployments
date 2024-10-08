fab58b00f821d4b2b51bb6d6f8f61df7199d4922
Nicky <90762430+BassBoulder@users.noreply.github.com>
2024-10-08 09:11:29 +0100

    Update Encoder.py

--- Azure Deployments/Encoder.py
+++ Azure Deployments/Encoder.py
@@ -1,26 +1,30 @@
+#Import OS & Parse Functions
 from os import replace
 import urllib.parse
 
+# Leave Unchanged - This is the URI to the Microsoft Portal
 Prefix = 'https://portal.azure.com/#create/Microsoft.Template/uri/'
 
-##ADD IN CONTAINER
-ContainerRoot = 'https://stuksazd.blob.core.windows.net/testarm/'
+# Change - This is the URI to your Storage Container
+ContainerRoot = '<Storage Container>'
 
-##ADD IN FILE
-ContainerFile = 'mg.json'
+# Change - This is the name of your File within the Storage Container
+ContainerFile = '<fileanme.json>'
 
-##ADD IN SAS
-SAS = 'sp=rl&st=2024-10-04T12:16:25Z&se=2024-11-08T21:16:25Z&spr=https&sv=2022-11-02&sr=c&sig=Oa732GZ9b8uuNBKN0pH3sjiJIPUvSvDEKacjWz%2F3Gto%3D'
+# Change - This is the SAS Key to your Storage Container (NO NOT LEAVE PUBLICLY AVAILABLE)
+SAS = '<SAS KEY>'
 
+
+# ENCODING PREPERATION
+#################
 FileAndSAS = (f'{ContainerFile}?{SAS}')
 
 EncodedRoot = urllib.parse.quote(ContainerRoot)
 EncodedFileAndSAS = urllib.parse.quote(FileAndSAS)
-
 ReplacedEncodedRoot = EncodedRoot.replace("/", "%2F")
-
-
+#################
+# ENCODING OUTPUT
 EncodedURL = (f'{Prefix}{ReplacedEncodedRoot}{EncodedFileAndSAS}')
 
-
+# Copy - This is what you need to copy into your "Deploy to Azure" button wrapper code
 print(EncodedURL)
